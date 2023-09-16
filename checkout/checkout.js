03793a7f07d8240c86afb7fe26f62cb4d1509d5d
crossfire3001 <obiwan2035@mail.ru>
2023-09-16 21:49:55 +0500

    working with status codes and JSON server

--- checkout/checkout.js
+++ checkout/checkout.js
@@ -19,12 +19,28 @@
 
   $("#redeem").click(function () {
     let url = "http://testologia.site/promo-code?name=" + $("#promo-code").val();
-    console.log(url);
 
     let http = new XMLHttpRequest();
     http.open("get", url);
     http.send();
 
+    http.onreadystatechange = () => {
+    if (http.readyState === 4 && http.status === 200)  {
+
+      let result = null;
+      try {
+        result = JSON.parse(http.responseText);
+      } catch (e) {}
+
+      // проверка на существование промокода
+      if (result) {
+        $("#promocode").removeClass("d-none").addClass("d-flex");
+      } else {
+
+      }
+
+    }
+    }
   })
 
 })()
